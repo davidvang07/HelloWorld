835952cef12f933de4e126a78ffd4c45f11aa310
davidvang07 <davidvang07@mail.fresnostate.edu>
2020-02-02 22:03:48 -0800

    adding Hello World source code

--- HelloWorld.ts
+++ HelloWorld.ts
@@ -1,5 +1,18 @@
 class HelloWorld {
-    constructor(parameters) {
-        
+
+    firstName = '';
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
-} Console console.log();
+
+    sayHello() {
+        console.log('Hello World!' + ' My name is ' + this.firstName + ' ' + this.lastName);
+    }
+
+}
+
+const myHelloClassInstance = new HelloWorld('David', 'Vang');
+myHelloClassInstance.sayHello();
